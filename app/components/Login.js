25cfc5f0436ff1b6bfb91a8088f666f18835e6e7
Garox <gabriel@garox.org>
2018-04-14 13:38:28 -0300

    shaping

--- app/components/Login.js
+++ app/components/Login.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
+import Sortable from 'sortablejs';
 import PropTypes from 'prop-types'
 
+
+const files = [ 'home.js', 'news.js', 'contact.js', 'about.js' ]
+
 export default class Login extends Component {
+  componentDidMount() {
+    var el = document.getElementById('toolbar');
+    var sortable = Sortable.create(el);
+  }
   render() {
     return (
       <div className="flexgrid">
         <div className="spacetool">
          <ul id="toolbar">
-          <li><a href="default.asp">Home</a></li>
-          <li><a href="news.asp">News</a></li>
-          <li><a href="contact.asp">Contact</a></li>
-          <li><a href="about.asp">About</a></li>
+         {files.map(file => {
+           return (
+           <li key={Math.random()}><a >{file}</a></li>
+          )
+         })}
           </ul> 
         </div>
         <div className="rest">
